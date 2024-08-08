6f44629bb315c4511a8278076b4f8367ae35a6d8
jeffplays2005 <jeffery20050811@gmail.com>
2024-08-08 16:28:23 +1200

    Edit Sign and Comment
    * Set the namespace as the main code block.
    
    * Set the PK to the Id of both classes.

--- Models/Sign.cs
+++ Models/Sign.cs
@@ -1,14 +1,17 @@
-namespace A1_jji134.Models;
+using System.ComponentModel.DataAnnotations;
 
-public class Sign {
-    /**
-    * The sign ID, uniquely identifies each sign based on an int.
-    * Primary key (PK)
-    */
-    public int Id { get; set;}
+namespace A1_jji134.Models {
+    public class Sign {
+        /**
+        * The sign ID, uniquely identifies each sign based on an int.
+        * Primary key (PK)
+        */
+        [Key]
+        public int Id { get; set;}
 
-    /**
-    * Contains the description of the sign.
-    */
-    public string Description { get; set;}
+        /**
+        * Contains the description of the sign.
+        */
+        public string Description { get; set;}
+    }
 }
