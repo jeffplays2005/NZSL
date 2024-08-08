ac0937ba6737cbe6b08c061fb7b351409ad55e08
jeffplays2005 <jeffery20050811@gmail.com>
2024-08-08 16:24:15 +1200

    Added Comment and Sign model

--- Models/Sign.cs
+++ Models/Sign.cs
@@ -1,1 +1,14 @@
+namespace A1_jji134.Models;
 
+public class Sign {
+    /**
+    * The sign ID, uniquely identifies each sign based on an int.
+    * Primary key (PK)
+    */
+    public int Id { get; set;}
+
+    /**
+    * Contains the description of the sign.
+    */
+    public string Description { get; set;}
+}
