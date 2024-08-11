37cc7031410a3fb8662c536f8fc91d0adf224cda
jeffplays2005 <jeffery20050811@gmail.com>
2024-08-12 10:45:01 +1200

    Add initial files for webapi controller

--- Controllers/A1Controller.cs
+++ Controllers/A1Controller.cs
@@ -1,1 +1,15 @@
-﻿
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace A1_jji134.Controllers
+{
+    [Route("webapi")]
+    [ApiController]
+    public class A1Controller : Controller
+    {
+        private readonly IWebAPIRepo _repository;
+        public A1Controller(IWebAPIRepo repository)
+        {
+            _repository = repository;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Hosting;
 using A1_jji134.Data;
 
 namespace A1_jji134
@@ -6,6 +7,11 @@ namespace A1_jji134
     {
         static void Main(string[] args)
         {  
+            var builder = WebApplication.CreateBuilder()
+            builder.Services.AddControllers();
+            
+            var app = builder.Build();
+            app.Run();
         }
     }
 }
