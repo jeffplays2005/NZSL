c5b289bd53256efbf1dad501579477bf365eddd3
jeffplays2005 <jeffery20050811@gmail.com>
2024-08-12 21:01:51 +1200

    14-database-crud-for-data-repo
    Create IA1Repo for repo interface structure.
    
    Add A1Repo according to the interface, created CRUD methods(excluding Update).
    
    Update DbContext to remove the override for configuration as this is included in Program.cs.
    
    Update Program.cs to include the builder options and important functions.

--- Data/A1DbContext.cs
+++ Data/A1DbContext.cs
@@ -5,12 +5,21 @@ namespace A1_jji134.Data
 {
     public class A1DbContext : DbContext
     {
+        // A1DbContext Constructor
+        public A1DbContext(DbContextOptions<A1DbContext> options) : base(options) {}
+
+        /**
+        * Comments database set.
+        */
         public DbSet<Comment> Comments { get; set; }
+        /**
+        * Signs database set.
+        */
         public DbSet<Sign> Signs { get; set; }
 
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            optionsBuilder.UseSqlite("Data Source=A1Database.sqlite");
-        }
+        // protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        // {
+        //     optionsBuilder.UseSqlite("Data Source=A1Database.sqlite");
+        // }
     }
 }
--- Program.cs
+++ Program.cs
@@ -6,6 +6,25 @@ namespace A1_jji134
     {
         static void Main(string[] args)
         {  
+            var builder = WebApplication.CreateBuilder(args);
+            // Registers endpoints that processes code.
+            builder.Services.AddControllers();
+            /**
+            * Register database context.
+            * Use builder configuration to fetch database location.
+            */
+            builder.Services.AddDbContext<A1DbContext>(options => 
+                options.UseSqlite(builder.Configuration["P1DBConnection"])
+            );
+            // Register implementation of A1Repo.
+            builder.Services.AddScoped<IA1Repo, A1Repo>();
+
+            var app = builder.Build();
+            // app.UseHttpsRedirection();
+            // app.UseAuthorization();
+            // app.MapControllers();
+
+            app.Run();
         }
     }
 }
