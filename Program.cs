e70246eb8c1c5120ffb55e86277b173e77bec50a
jeffplays2005 <jeffery20050811@gmail.com>
2024-08-12 13:42:16 +1200

    17-database-link-dbcontext-to-main-program

--- Program.cs
+++ Program.cs
@@ -5,7 +5,11 @@ namespace A1_jji134
     class Program
     {
         static void Main(string[] args)
-        {  
+        {
+            var builder = WebApplication.CreateBuilder()
+            builder.Services.addDbContext<A1DbContext>(options => 
+                options.UseSqlite(builder.Configuration["WebAPIConnection"])
+            );
         }
     }
 }
