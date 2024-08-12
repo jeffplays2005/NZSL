d750947b486b56b2c7461420303f50a6dcdce4ec
Jeffery <61447509+jeffplays2005@users.noreply.github.com>
2024-08-12 22:07:58 +1200

    14-database-crud-for-data-repo (#19)
    * 14-database-crud-for-data-repo
    
    Create IA1Repo for repo interface structure.
    
    Add A1Repo according to the interface, created CRUD methods(excluding Update).
    
    Update DbContext to remove the override for configuration as this is included in Program.cs.
    
    Update Program.cs to include the builder options and important functions.
    
    * Fix errors in names and type declarations
    
    Fixed naming issue with db context from template name.
    
    Also include microsoft sqlite package to remove main.cs problems.

--- Data/A1Repo.cs
+++ Data/A1Repo.cs
@@ -1,1 +1,58 @@
-﻿
+﻿using Microsoft.EntityFrameworkCore.ChangeTracking;
+using A1_jji134.Models;
+
+namespace A1_jji134.Data
+{
+    public class A1Repo : IA1Repo
+    {
+        /**
+        * Local DB context reference, injected through the constructor.
+        */
+        private readonly A1DbContext _dbContext;
+        /**
+        * DB Context constructor
+        * @params A1DbContext The DB context.
+        */
+        public A1Repo(A1DbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        /**
+        * Implement comment database methods
+        */
+        public IEnumerable<Comment> GetAllComments()
+        {
+            return _dbContext.Comments.ToList();
+        }
+        public Comment GetCommentByID(int id)
+        {
+            return _dbContext.Comments.FirstOrDefault(c => c.Id == id);
+        }
+        public Comment AddComment(Comment comment)
+        {
+            EntityEntry<Comment> e = _dbContext.Comments.Add(comment);
+            Comment c = e.Entity;
+            _dbContext.SaveChanges();
+            return c;
+        }
+
+        /**
+        * Implement sign database methods
+        */
+         public IEnumerable<Sign> GetAllSigns()
+        {
+            return _dbContext.Signs.ToList();
+        }
+        public Sign GetSignByID(int id)
+        {
+            return _dbContext.Signs.FirstOrDefault(s => s.Id == id);
+        }
+        public Sign AddSign(Sign sign)
+        {
+            EntityEntry<Sign> e = _dbContext.Signs.Add(sign);
+            Sign s = e.Entity;
+            _dbContext.SaveChanges();
+            return s;
+        }
+    }
+}
--- Data/IA1Repo.cs
+++ Data/IA1Repo.cs
@@ -1,1 +1,16 @@
-﻿
+﻿using A1_jji134.Models;
+
+namespace A1_jji134.Data
+{
+    public interface IA1Repo {
+        // Comment database actions
+        IEnumerable<Comment> GetAllComments();
+        Comment GetCommentByID(int id);
+        Comment AddComment(Comment comment);
+
+        // Sign database actions
+        IEnumerable<Sign> GetAllSigns();
+        Sign GetSignByID(int id);
+        Sign AddSign(Sign sign);
+    }
+}
