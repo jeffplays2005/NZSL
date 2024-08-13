3aa2a89d33c19111639709ac9083c57999e0616b
Jeffery <61447509+jeffplays2005@users.noreply.github.com>
2024-08-13 18:03:11 +1200

    8-controller-create-controller-for-webapi (#16)
    * Add initial files for webapi controller
    
    * Add GetVersion endpoint to A1Controller

--- Controllers/A1Controller.cs
+++ Controllers/A1Controller.cs
@@ -1,1 +1,23 @@
-﻿
+using Microsoft.AspNetCore.Mvc;
+using A1_jji134.Data;
+
+namespace A1_jji134.Controllers
+{
+    [Route("webapi")]
+    [ApiController]
+    public class A1Controller : Controller
+    {
+        private readonly IA1Repo _repository;
+
+        public A1Controller(IA1Repo repository)
+        {
+            _repository = repository;
+        }
+
+        [HttpGet("GetVersion")]
+        public ActionResult GetVersion()
+        {
+            return Ok("1.0.0 (Ngāruawāhia) by jji134");
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
 using A1_jji134.Data;
 
@@ -6,25 +7,24 @@ namespace A1_jji134
     class Program
     {
         static void Main(string[] args)
-        {  
+        {
             var builder = WebApplication.CreateBuilder(args);
-            // Registers endpoints that processes code.
-            builder.Services.AddControllers();
             /**
             * Register database context.
             * Use builder configuration to fetch database location.
             */
-            builder.Services.AddDbContext<A1DbContext>(options => 
+            builder.Services.AddDbContext<A1DbContext>(options =>
                 options.UseSqlite(builder.Configuration["P1DBConnection"])
             );
+            // Registers endpoints that processes code.
+            builder.Services.AddControllers();
             // Register implementation of A1Repo.
             builder.Services.AddScoped<IA1Repo, A1Repo>();
 
             var app = builder.Build();
             // app.UseHttpsRedirection();
             // app.UseAuthorization();
-            // app.MapControllers();
-
+            app.MapControllers();
             app.Run();
         }
     }
