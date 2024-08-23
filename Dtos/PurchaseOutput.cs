e2a484f93c39e8e24c1311f443ed314214d6f002
Jeffery <61447509+jeffplays2005@users.noreply.github.com>
2024-08-24 09:20:54 +1200

    Added dto (#39)

--- Dtos/PurchaseOutput.cs
+++ Dtos/PurchaseOutput.cs
@@ -1,1 +1,13 @@
-﻿
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace A2_jji134.Dtos 
+{
+    public class PurchaseOutputDto
+    {
+        [Required]
+        public string UserName { get; set; }
+
+        [Required]
+        public string SignID { get; set; }
+    }
+}
