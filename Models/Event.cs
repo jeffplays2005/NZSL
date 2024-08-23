fa1870657657d6fd8c622ff71202ce284188ca2d
Jeffery <61447509+jeffplays2005@users.noreply.github.com>
2024-08-24 00:04:55 +1200

    Added 4 models for A2 (#34)
    Event, Organizer, Sign, User

--- Models/Event.cs
+++ Models/Event.cs
@@ -1,1 +1,20 @@
-﻿
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace A2_jji134.Models
+{
+    public class Event {
+        [Key]
+        public int Id { get; set; }
+        
+        [Required]
+        public string Start { get; set; } // yyyyMMddTHHmmssZ
+        [Required]
+        public string End { get; set; } // yyyyMMddTHHmmssZ
+        [Required]
+        public string Summary { get; set; }
+        [Required]
+        public string Description { get; set; }
+        [Required]
+        public string Location { get; set; }
+    }
+}
