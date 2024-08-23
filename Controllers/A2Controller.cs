55e8fba96b703c1c09d6922f2abcda71872872ea
Jeffery <61447509+jeffplays2005@users.noreply.github.com>
2024-08-24 09:16:49 +1200

    35 create a2 dbrepo interface repo and dbcontext (#37)
    * Add DbContext
    
    * Added IA2Repo interface for data repo
    
    * Add A2Repo and fix bugs
    
    Fixed broken usings and publicitys.
    
    * Changes to Program.cs and routes

--- Controllers/A2Controller.cs
+++ Controllers/A2Controller.cs
@@ -1,1 +1,32 @@
-﻿
+﻿using A2_jji134.Data;
+using A2_jji134.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace A2_jji134.Controllers
+{
+    [Route("webapi")]
+    [ApiController]
+    public class A2Controller : Controller
+    {
+        private readonly IA2Repo _repo;
+        public A2Controller(IA2Repo repo)
+        {
+            _repo = repo;
+        }
+        [HttpPost("Register")]
+        public ActionResult Register(User user)
+        {
+            // Check if already is a user
+            User userCheck = _repo.GetUserByName(user.UserName);
+            if (userCheck == null)
+            {
+                _repo.AddUser(user);
+                return Ok("User successfully registered.");
+            } else {
+                return Ok($"UserName {user.UserName} is not available.");
+            }
+        }
+
+        
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using A2_jji134.Data;
 
 namespace A2_jji134
 {
@@ -7,8 +9,19 @@ namespace A2_jji134
         static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
-
+            builder.Services.AddDbContext<A2DbContext>(options => 
+                options.UseSqlite(builder.Configuration["A2DBConnection"])
+            );
+            builder.Services.AddControllers();
+            builder.Services.AddScoped<IA2Repo, A2Repo>();
+            builder.Services.AddSwaggerGen();
             var app = builder.Build();
+            app.MapControllers();
+            if(app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
 
             app.Run();
         }
