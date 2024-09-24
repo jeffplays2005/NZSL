70cd9006d64e2f756abf516e5500faa02a6acba7
jeffplays2005 <jeffery20050811@gmail.com>
2024-09-24 22:06:01 +1200

    Add version methods and also calendar parser method

--- jji134.js
+++ jji134.js
@@ -1,7 +1,20 @@
-const input = document.getElementById("search-bar");
+/**
+ * Version methods
+ */
+async function getVersion() {
+  const response = await fetch(`https://cws.auckland.ac.nz/nzsl/api/Version`);
+  const data = await response.text();
+  return data;
+}
+function showVersion() {
+  getVersion().then((data) => {
+    document.getElementById("version").innerText = data;
+  });
+}
 
-input.addEventListener("input", fetchNZSLsigns);
-
+/**
+ * Sign methods
+ */
 async function fetchNZSLsigns(term) {
   let response;
   if (!term) {
@@ -19,6 +32,9 @@ async function fetchNZSLsigns(term) {
   });
 }
 
+/**
+ * Event methods
+ */
 async function getNumberEvents() {
   const response = await fetch(
     `https://cws.auckland.ac.nz/nzsl/api/EventCount`,
@@ -41,10 +57,32 @@ async function getEvents(eventCount) {
   }
   return eventOutput;
 }
-async function getVersion() {
-  const response = await fetch(`https://cws.auckland.ac.nz/nzsl/api/Version`);
-  const data = await response.text();
-  return data;
+
+//0: BEGIN:VCALENDAR
+//1: VERSION:2.0
+//2: PRODID:-//NSDS//EN
+//3: BEGIN:VEVENT
+//4: UID:20240903T010000Z@NZDS
+//5: DTSTAMP:20240903T010000Z
+//6: DTSTART:20240903T010000Z
+//7: DTEND:20240903T020000Z
+//8: TZID:Pacific/Auckland
+//9: SUMMARY:Colours Galore
+//10: DESCRIPTION:This event is to celbrate the colours in our lives. Bring the Whānau and plenty of pastels.
+//11: LOCATION:Ōtāhuhu
+// END:VEVENT
+// END:VCALENDAR
+function parseVcalendar(calendar) {
+  const splitted = calendar.split("\n").map((x) => x.split(":")[1]);
+  return {
+    uid: splitted[4],
+    start: splitted[6],
+    end: splitted[7],
+    timezone: splitted[8],
+    summary: splitted[9],
+    description: splitted[10],
+    lcocation: splitted[11],
+  };
 }
 
 // if (response.status == 200) {
@@ -57,3 +95,8 @@ async function getVersion() {
 //   signElement.textContent = `${data.map((sign) => `${sign.description}`).join("\n")}`;
 //   signElement.textContent = ``;
 // }
+
+// const input = document.getElementById("search-bar");
+
+// input.addEventListener("input", fetchNZSLsigns);
+showVersion();
