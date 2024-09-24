01cfff3e865bf7a8b5f7bea7638c0944daf704d7
jeffplays2005 <jeffery20050811@gmail.com>
2024-09-24 17:25:41 +1200

    Add event methods to get number and get events

--- jji134.js
+++ jji134.js
@@ -11,11 +11,41 @@ async function fetchNZSLsigns(term) {
   }
   const data = await response.json();
   return data.map((sign) => {
-    return { description: sign.description, id: sign.id };
+    return {
+      description: sign.description,
+      id: sign.id,
+      image: `https://cws.auckland.ac.nz/nzsl/api/SignImage/${sign.id}`,
+    };
   });
 }
 
-async function fetchSignLogo() {}
+async function getNumberEvents() {
+  const response = await fetch(
+    `https://cws.auckland.ac.nz/nzsl/api/EventCount`,
+  );
+  const data = await response.text();
+  return parseInt(data.result);
+}
+
+async function getEvents(eventCount) {
+  eventOutput = [];
+  for (let i = 0; i < eventCount; i++) {
+    const response = await fetch(
+      `https://cws.auckland.ac.nz/nzsl/api/Event/1`,
+      {
+        headers: { Accept: "text/plain" },
+      },
+    );
+    const data = await response.text();
+    eventOutput.push(data);
+  }
+  return eventOutput;
+}
+async function getVersion() {
+  const response = await fetch(`https://cws.auckland.ac.nz/nzsl/api/Version`);
+  const data = await response.text();
+  return data;
+}
 
 // if (response.status == 200) {
 //   data = await response.json();
