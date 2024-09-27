075697f8166b65673d1705d7bcc4209f5e3a836c
Jeffery <61447509+jeffplays2005@users.noreply.github.com>
2024-09-27 17:46:07 +1200

    Create html sections (#49)
    * create-html-sections
    
    Create search and main section
    
    * Fix search not clearing
    
    * Fix navbar css
    
    * Add events logic and code to display
    
    * Neaten and display events in a flex box
    
    * Remove other files
    
    * Add create comment functionality and form
    
    * Add logout functionality
    
    * Add register and login
    
    * Fix errors showing because of the same id

--- jji134.js
+++ jji134.js
@@ -1,3 +1,4 @@
+const CREDENTIAL_KEY = "credentials";
 /**
  * Version methods
  */
@@ -32,6 +33,23 @@ async function fetchNZSLsigns(term) {
     };
   });
 }
+function formatSigns(signs) {
+  return signs
+    .map((sign) => {
+      return `<div class="sign">
+      <div>
+        ${sign.id}
+        ${sign.description}
+      </div>
+      <img src="${sign.image}"/>
+    </div>`;
+    })
+    .join("\n");
+}
+function displaySigns(signHTML) {
+  const signDiv = document.getElementById("nzsl-signs");
+  signDiv.innerHTML = signHTML;
+}
 
 /**
  * Event methods
@@ -41,49 +59,82 @@ async function getNumberEvents() {
     `https://cws.auckland.ac.nz/nzsl/api/EventCount`,
   );
   const data = await response.text();
-  return parseInt(data.result);
+  return parseInt(data);
 }
-
-async function getEvents(eventCount) {
-  eventOutput = [];
-  for (let i = 0; i < eventCount; i++) {
-    const response = await fetch(
-      `https://cws.auckland.ac.nz/nzsl/api/Event/1`,
-      {
-        headers: { Accept: "text/plain" },
-      },
-    );
-    const data = await response.text();
-    eventOutput.push(data);
-  }
-  return eventOutput;
+async function getEvent(eventID) {
+  const response = await fetch(
+    `https://cws.auckland.ac.nz/nzsl/api/Event/${eventID}`,
+    {
+      headers: { Accept: "text/plain" },
+    },
+  );
+  const data = await response.text();
+  return data;
 }
-
-//0: BEGIN:VCALENDAR
-//1: VERSION:2.0
-//2: PRODID:-//NSDS//EN
-//3: BEGIN:VEVENT
-//4: UID:20240903T010000Z@NZDS
-//5: DTSTAMP:20240903T010000Z
-//6: DTSTART:20240903T010000Z
-//7: DTEND:20240903T020000Z
-//8: TZID:Pacific/Auckland
-//9: SUMMARY:Colours Galore
-//10: DESCRIPTION:This event is to celbrate the colours in our lives. Bring the Whānau and plenty of pastels.
-//11: LOCATION:Ōtāhuhu
-// END:VEVENT
-// END:VCALENDAR
-function parseVcalendar(calendar) {
+function functionParseDate(dateString) {
+  // 2024-09-03T01:00:00Z
+  const year = dateString.substring(0, 4); // 2024
+  const month = dateString.substring(4, 6); // 09
+  const day = dateString.substring(6, 8); // 03
+  const hour = dateString.substring(9, 11); // 01
+  const minutes = dateString.substring(11, 13); // 00
+  const seconds = dateString.substring(13, 15); // 00
+  const newDate = new Date(
+    `${year}-${month}-${day}T${hour}:${minutes}:${seconds}Z`,
+  );
+  return newDate.toLocaleString();
+}
+function parseVcalendar(calendar, id) {
   const splitted = calendar.split("\n").map((x) => x.split(":")[1]);
   return {
-    uid: splitted[4],
-    start: splitted[6],
-    end: splitted[7],
+    start: functionParseDate(splitted[6]),
+    end: functionParseDate(splitted[7]),
     timezone: splitted[8],
     summary: splitted[9],
     description: splitted[10],
     lcocation: splitted[11],
+    url: `https://cws.auckland.ac.nz/nzsl/api/Event/${id}`,
   };
+}
+async function getEvents(eventCount) {
+  const eventOutput = [];
+  for (let i = 0; i < eventCount; i++) {
+    const fetchedEvent = await getEvent(i);
+    const parsedCalendar = parseVcalendar(fetchedEvent, i);
+    eventOutput.push(parsedCalendar);
+  }
+  return eventOutput;
+}
+async function displayEvents() {
+  // Fetch event count
+  const eventCount = await getNumberEvents();
+  // Fetch all events and returns a list of parsed events
+  const allEvents = await getEvents(eventCount);
+  // Convert the data to HTML
+  const formattedEventHTML = formatEvents(allEvents);
+  // Display the events html
+  const eventDiv = document.getElementById("nzsl-events");
+  eventDiv.innerHTML = formattedEventHTML;
+}
+function formatEvents(events) {
+  return events
+    .map((event) => {
+      return `<div class="event">
+      <h2>${event.summary}</h2>
+      <a href="${event.url}">
+        <button>Click to download event</button>
+      </a>
+      <br/>
+      <strong>Description</strong>: ${event.description}
+      <br/>
+      <strong>Location</strong>: ${event.lcocation}
+      <br/>
+      <strong>Starts</strong>: ${event.start}
+      <br/>
+      <strong>Ends</strong>: ${event.end}
+  </div>`;
+    })
+    .join("\n");
 }
 
 /**
@@ -94,21 +145,49 @@ async function getAllComments() {
   const data = await response.text();
   return data;
 }
-function displayComments() {}
-async function addComment(comment) {
-  const response = await fetch(
-    `https://cws.auckland.ac.nz/nzsl/api/Comment?comment=${comment}`,
-    {
-      method: "POST",
-      headers: {
-        Authorization: `Basic ${getSession("credentials")}`,
-      },
-    },
-  );
-  const data = await response.text();
-  return data;
+async function displayComments() {
+  const commentsDiv = document.getElementById("nzsl-comments");
+  commentsDiv.innerHTML = await getAllComments();
 }
-
+async function addComment(comment) {
+  try {
+    const response = await fetch(
+      `https://cws.auckland.ac.nz/nzsl/api/Comment?comment=${comment}`,
+      {
+        method: "POST",
+        headers: {
+          Authorization: `Basic ${getSession(CREDENTIAL_KEY)}`,
+        },
+      },
+    );
+    const data = await response.text();
+    return data;
+  } catch (e) {
+    throw new Error("An error occurred.");
+  }
+}
+async function submitComment(event) {
+  event.preventDefault();
+  // Check for auth
+  if (getSession(CREDENTIAL_KEY) == null) {
+    return showSection("login");
+  }
+  const form = event.target;
+  const formData = new FormData(form);
+  // Convert form data to an object (optional, for easier use)
+  const data = {};
+  formData.forEach((value, key) => {
+    data[key] = value;
+  });
+  try {
+    await addComment(data.comment);
+    return showSection("guest-book");
+  } catch (e) {
+    showSection("guest-book");
+    console.log("an error");
+    document.getElementById("comment-errors").innerText = "An error occurred.";
+  }
+}
 /**
  * Auth methods
  */
@@ -129,21 +208,66 @@ async function register(username, password, address) {
   // This endpoint returns 200 regardless of conflicts or success
   return data;
 }
-async function login(username, password) {
-  const response = await fetch(`https://cws.auckland.ac.nz/nzsl/api/TestAuth`, {
-    headers: {
-      Authorization: `Basic ${convertToBase64(username, password)}`,
-    },
+async function submitRegister(event) {
+  event.preventDefault();
+  // Check for auth
+  const form = event.target;
+  const formData = new FormData(form);
+  // Convert form data to an object (optional, for easier use)
+  const data = {};
+  formData.forEach((value, key) => {
+    data[key] = value;
   });
-  if (response.status !== 200) {
-    throw new Error("Invalid login. ");
+  const output = await register(data.username, data.password, data.address);
+  console.log(output);
+  if (output == "Username not available") {
+    showSection("register");
+    document.getElementById("register-errors").innerText =
+      "Username not available";
+  } else {
+    showSection("login");
+    document.getElementById("success").innerText = "Successfully registered!";
   }
-  // Set cookies or something
-  storeSession("credentials", convertToBase64(username, password));
-  return true;
 }
-function logout() {
-  return clearSession();
+async function login(username, password) {
+  try {
+    const response = await fetch(
+      `https://cws.auckland.ac.nz/nzsl/api/TestAuth`,
+      {
+        headers: {
+          Authorization: `Basic ${convertToBase64(username, password)}`,
+        },
+      },
+    );
+    if (response.status === 200) {
+      return true;
+    }
+  } catch (e) {}
+  return false;
+}
+async function submitLogin(event) {
+  event.preventDefault();
+  // Check for auth
+  const form = event.target;
+  const formData = new FormData(form);
+  // Convert form data to an object (optional, for easier use)
+  const data = {};
+  formData.forEach((value, key) => {
+    data[key] = value;
+  });
+  const output = await login(data.username, data.password);
+  if (output) {
+    storeSession(CREDENTIAL_KEY, convertToBase64(data.username, data.password));
+    showSection("home");
+  } else {
+    showSection("login");
+    document.getElementById("login-errors").innerText = "Invalid login.";
+  }
+}
+
+async function logout() {
+  await clearSession();
+  return showSection("login");
 }
 function convertToBase64(username, password) {
   return btoa(`${username}:${password}`);
@@ -154,7 +278,7 @@ function storeSession(key, value) {
 function getSession(key) {
   return localStorage.getItem(key);
 }
-function clearSession() {
+async function clearSession() {
   return localStorage.clear();
 }
 // if (response.status == 200) {
@@ -168,7 +292,67 @@ function clearSession() {
 //   signElement.textContent = ``;
 // }
 
-// const input = document.getElementById("search-bar");
+async function showSection(section) {
+  const sections = document.querySelectorAll("section");
+  sections.forEach((sect) => {
+    sect.style.display = "none";
+  });
+  const forms = document.querySelectorAll("form");
+  forms.forEach((form) => {
+    form.reset();
+  });
+  if (section == "nzsl") {
+    // Reset the search bar
+    const searchBar = document.getElementById("nzsl-search");
+    searchBar.value = "";
+    // Fetch all the signs
+    const signs = await fetchNZSLsigns("");
+    const formattedSigns = formatSigns(signs);
+    displaySigns(formattedSigns);
+  }
+  if (section == "events") {
+    displayEvents();
+  }
+  if (section == "guest-book") {
+    displayComments();
+  }
+  // Display the section requested
+  document.getElementById(section).style.display = "block";
+  // Hide all authenticated stuff
+  if (getSession(CREDENTIAL_KEY) !== null) {
+    document.getElementById("login-li").style.display = "none";
+    document.getElementById("register-li").style.display = "none";
+    document.getElementById("logout-li").style.display = "block";
+  } else {
+    document.getElementById("login-li").style.display = "block";
+    document.getElementById("register-li").style.display = "block";
+    document.getElementById("logout-li").style.display = "none";
+  }
+  // Reset all errors
+  document.getElementById("login-errors").innerText = "";
+  document.getElementById("register-errors").innerText = "";
+  document.getElementById("comment-errors").innerText = "";
+  document.getElementById("success").innerText = "";
+}
 
-// input.addEventListener("input", fetchNZSLsigns);
+function createSearchListener() {
+  const input = document.getElementById("nzsl-search");
+  input.addEventListener("input", async (event) => {
+    const term = event.target.value;
+    const signs = await fetchNZSLsigns(term);
+    const formattedSigns = formatSigns(signs);
+    displaySigns(formattedSigns);
+  });
+}
+// Show version at footer
 showVersion();
+// Add searchbar listener
+createSearchListener();
+// Hide all authd stuff
+document.getElementById("logout-li").style.display = "none";
+if (getSession(CREDENTIAL_KEY) !== null) {
+  document.getElementById("login-li").style.display = "none";
+  document.getElementById("register-li").style.display = "none";
+  document.getElementById("logout-li").style.display = "block";
+}
+showSection("home");
